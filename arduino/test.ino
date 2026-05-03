6863fd7be72d7aa872cae49074e0d88f22b05696
Alex <151869877+Alex-lrd@users.noreply.github.com>
2026-05-03 14:33:32 +0200

    1 Capteur
    Refactor serial communication and add command handling for start/stop scanning.

--- arduino/test.ino
+++ arduino/test.ino
@@ -1,16 +1,52 @@
+bool scan = false;
+float speed = 1.0;
+float timePropagation = .0343;
+
+const int trigPin = A0;  
+const int echoPin = A1;
+float duration, distance;  
+
+String cmd = "";
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Important 9600baud pas 115200 sinon corruption data
+  delay(2000);
+
+  Serial.println("PulseX Ready !!!");
+
+  pinMode(trigPin, OUTPUT);  
+	pinMode(echoPin, INPUT); 
 }
 
 void loop() {
-  // Envoyer une donnée
-  Serial.println("Bonjour depuis Arduino !");
-  delay(1000);
 
-  // Lire ce que Python envoie
-  if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    Serial.print("Reçu : ");
-    Serial.println(data);
+  if (Serial.available()) {
+    cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+
+    Serial.print("RECU:");
+    Serial.println(cmd);
+
+    if (cmd == "start") {
+      // Serial.println("OK START");
+      scan = true;
+    } else if (cmd == "stop") {
+      // Serial.println("OK STOP");
+      scan = false;
+    } 
   }
+
+
+  if (scan) {
+    digitalWrite(trigPin, LOW);  
+    delayMicroseconds(2);  
+    digitalWrite(trigPin, HIGH);  
+    delayMicroseconds(10);  
+    digitalWrite(trigPin, LOW); 
+
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration * timePropagation) / 2;
+    Serial.println(distance);
+  }
+
 }
